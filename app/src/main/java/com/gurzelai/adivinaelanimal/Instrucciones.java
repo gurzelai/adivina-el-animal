12c53d64f34b52ec34fab3d935a17aef017d3036
gurzelai <gorkaurzelai2000@gmail.com>
2021-03-14 20:34:21 +0100

    analitics entrar en instrucciones

--- app/src/main/java/com/gurzelai/adivinaelanimal/Instrucciones.java
+++ app/src/main/java/com/gurzelai/adivinaelanimal/Instrucciones.java
@@ -2,8 +2,11 @@ package com.gurzelai.adivinaelanimal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class Instrucciones extends AppCompatActivity {
 
@@ -13,6 +16,11 @@ public class Instrucciones extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instrucciones);
+
+        FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(this);
+        Bundle bundle = new Bundle();
+        bundle.putString("a", "Ha entrado en informacion");
+        analytics.logEvent("a", bundle);
 
         volver = (Button) findViewById(R.id.boton);
         volver.setOnClickListener(view -> finish());
